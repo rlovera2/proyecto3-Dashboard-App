e8b9b39c483c6b018fb855c5e16ce0822fad7a42
rlovera2 <125207616+rlovera2@users.noreply.github.com>
2023-07-03 22:10:17 -0400

    Actualizar llamadasApis.js

--- src/js/llamadasApis.js
+++ src/js/llamadasApis.js
@@ -1,11 +1,22 @@
 
 import { imprimirValores, pintarGrafica } from "./pintar.js";
 
-const datosApi
+notasAlumnos ={
+    id:1,
+    nombre: Pedro,
+    calificación: 15
+
+};
+
+
+
+const datosApi[notasAlumnos];
 
 const consumirApi = async () => {
+
+const resp = await axios.get(datosApi);
   
-  const resp = await axios.get("https://ucamp-api.onrender.com/api/v1/scores");
+  //const resp = await axios.get("https://ucamp-api.onrender.com/api/v1/scores");
     
  
 
